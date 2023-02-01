90eeb3719cc4bc62477fb596659ecbe5575f6477
tristan <tristan@techmate.org.nz>
2023-02-01 18:55:34 +1300

    prettier formatting

--- email-composer.sh
+++ email-composer.sh
@@ -12,11 +12,14 @@ BODY+="Backup Logs for last 7 entries follows:\n"
 BODY+="$(cat $DIR_PATH/backup.log | tail -7)\n"
 # check if any errors thrown by rclone  
 if [ -s $DIR_PATH/error.log ]; then
-	BODY+="\n...\nError Log:\n\n"
+	BODY+="\n\nError Log:\n\n"
 	BODY+=$(cat $DIR_PATH/error.log)
 else
-	BODY+="\n...\nrclone reports no errors\n\n"
+	BODY+="\n----------------------------------"
+	BODY+="\n\nrclone reports no errors\n"
+	BODY+="\n----------------------------------"
+	
 fi
-BODY+="Your thought for the day:\n"
+BODY+="\n\nYour thought for the day:\n\n"
 BODY+=$(fortune)
 echo -e "$TO\n$SUBJECT\n$BODY" > $DIR_PATH/auto-email.txt
